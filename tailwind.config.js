9d2b55454fb92814d9067dba196bd984a9c2b344
Prajay Reddy Minuka <54284068+mprajay999@users.noreply.github.com>
2025-02-18 21:43:28 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#333333',
+        secondary: '#E4D6B3',
+        accent: '#A68DAD',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        opensans: ['Open Sans', 'sans-serif'],
+        playfair: ['Playfair Display', 'serif'],
+      },
+    },
   },
   plugins: [],
-};
+}
